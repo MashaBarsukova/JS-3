b56b4e37d13220ac93694a30e6cd10ba283e71e1
MashaBarsukova <marybarskova@gmail.com>
2018-02-22 13:57:25 +0200

    add files

---
+++ js/function.js
@@ -0,0 +1,16 @@
+const keyboard = [
+  ["q","w","e","r","t","y","u","i","o","p","{","}"],
+  ["a","s","d","f","g","h","j","k","l",":","''"],
+  ["z","x","c","v","b","n","m",",",".","/"]
+];
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm/";
+const hello = keyboard[1][5] + keyboard[0][2] + keyboard[1][8] + keyboard[1][8] +
+keyboard[0][8];
+console.log(hello);
+const javascript = keyboard[1][6] + keyboard[1][0] + keyboard[2][3] + keyboard[1][0] +
+keyboard[1][1] + keyboard[2][2] + keyboard[0][3] + keyboard[0][7] + keyboard[0][9] +
+keyboard[0][4];
+console.log(javascript);
+const trainer = keyboard[0][4] + keyboard[0][3] + keyboard[1][0] + keyboard[0][7] +
+keyboard[2][5] + keyboard[0][2] + keyboard[0][3];
+console.log(trainer);
